40a271b01107df06433e0a2b8536032e931e532c
Matias Grioni <matgrioni@gmail.com>
2018-05-25 09:30:19 -0400

    Update interfaces for conversion methods and move character maps to submodule.

--- betacode/conv.py
+++ betacode/conv.py
@@ -3,6 +3,8 @@ import unicodedata
 
 import pygtrie
 
+from . import _map
+
 # Special characters that need their own references to rewrite with
 _FINAL_LC_SIGMA = '\u03c2'
 _MEDIAL_LC_SIGMA = '\u03c3'
@@ -10,382 +12,89 @@ _MEDIAL_LC_SIGMA = '\u03c3'
 # Punctuation marks in the betacode map
 _BETA_PUNCTUATION = frozenset('\':-_')
 
-_BETACODE_MAP = {
-    # No marks
-    'a':       '\u03b1',
-    'b':       '\u03b2',
-    'g':       '\u03b3',
-    'd':       '\u03b4',
-    'e':       '\u03b5',
-    'z':       '\u03b6',
-    'h':       '\u03b7',
-    'q':       '\u03b8',
-    'i':       '\u03b9',
-    'k':       '\u03ba',
-    'l':       '\u03bb',
-    'm':       '\u03bc',
-    'n':       '\u03bd',
-    'c':       '\u03be',
-    'o':       '\u03bf',
-    'p':       '\u03c0',
-    'r':       '\u03c1',
-    's':       '\u03c3',
-    's1':      '\u03c3',
-    's2':      '\u03c2',
-    's3':      '\u03f2',
-    't':       '\u03c4',
-    'u':       '\u03c5',
-    'f':       '\u03c6',
-    'x':       '\u03c7',
-    'y':       '\u03c8',
-    'w':       '\u03c9',
-    '*a':      '\u0391',
-    '*b':      '\u0392',
-    '*g':      '\u0393',
-    '*d':      '\u0394',
-    '*e':      '\u0395',
-    '*z':      '\u0396',
-    '*h':      '\u0397',
-    '*q':      '\u0398',
-    '*i':      '\u0399',
-    '*k':      '\u039a',
-    '*l':      '\u039b',
-    '*m':      '\u039c',
-    '*n':      '\u039d',
-    '*c':      '\u039e',
-    '*o':      '\u039f',
-    '*p':      '\u03a0',
-    '*r':      '\u03a1',
-    '*s':      '\u03a3',
-    '*s3':     '\u03f9',
-    '*t':      '\u03a4',
-    '*u':      '\u03a5',
-    '*f':      '\u03a6',
-    '*x':      '\u03a7',
-    '*y':      '\u03a8',
-    '*w':      '\u03a9',
-
-    # Smooth breathing
-    'a)':      '\u1f00',
-    'e)':      '\u1f10',
-    'h)':      '\u1f20',
-    'i)':      '\u1f30',
-    'o)':      '\u1f40',
-    'u)':      '\u1f50',
-    'w)':      '\u1f60',
-    'r)':      '\u1fe4',
-    '*)a':     '\u1f08',
-    '*)e':     '\u1f18',
-    '*)h':     '\u1f28',
-    '*)i':     '\u1f38',
-    '*)o':     '\u1f48',
-    '*)w':     '\u1f68',
-
-    # Rough breathing
-    'a(':      '\u1f01',
-    'e(':      '\u1f11',
-    'h(':      '\u1f21',
-    'i(':      '\u1f31',
-    'o(':      '\u1f41',
-    'u(':      '\u1f51',
-    'w(':      '\u1f61',
-    'r(':      '\u1fe5',
-    '*(a':     '\u1f09',
-    '*(e':     '\u1f19',
-    '*(h':     '\u1f29',
-    '*(i':     '\u1f39',
-    '*(o':     '\u1f49',
-    '*(u':     '\u1f59',
-    '*(w':     '\u1f69',
-    '*(r':     '\u1fec',
-
-    # Acute accent and grave accent
-    'a\\':     '\u1f70',
-    'a/':      '\u1f71',
-    'e\\':     '\u1f72',
-    'e/':      '\u1f73',
-    'h\\':     '\u1f74',
-    'h/':      '\u1f75',
-    'i\\':     '\u1f76',
-    'i/':      '\u1f77',
-    'o\\':     '\u1f78',
-    'o/':      '\u1f79',
-    'u\\':     '\u1f7a',
-    'u/':      '\u1f7b',
-    'w\\':     '\u1f7c',
-    'w/':      '\u1f7d',
-    '*\\a':    '\u1fba',
-    '*/a':     '\u1fbb',
-    '*\\e':    '\u1fce',
-    '*/e':     '\u1fc9',
-    '*\\h':    '\u1fca',
-    '*/h':     '\u1fcb',
-    '*\\i':    '\u1fda',
-    '*/i':     '\u1fdb',
-    '*\\o':    '\u1ff8',
-    '*/o':     '\u1ff9',
-    '*\\u':    '\u1fea',
-    '*/u':     '\u1feb',
-    '*\\w':    '\u1ffa',
-    '*/w':     '\u1ffb',
-
-    # Smooth breathing and acute accent
-    'a)/':     '\u1f04',
-    'e)/':     '\u1f14',
-    'h)/':     '\u1f24',
-    'i)/':     '\u1f34',
-    'o)/':     '\u1f44',
-    'u)/':     '\u1f54',
-    'w)/':     '\u1f64',
-    '*)/a':    '\u1f0c',
-    '*)/e':    '\u1f1c',
-    '*)/h':    '\u1f2c',
-    '*)/i':    '\u1f3c',
-    '*)/o':    '\u1f4c',
-    '*)/u':    '\u1f5c',
-    '*)/w':    '\u1f6c',
-
-    # Smooth breathing and grave accent
-    'a)\\':    '\u1f02',
-    'e)\\':    '\u1f12',
-    'h)\\':    '\u1f22',
-    'i)\\':    '\u1f32',
-    'o)\\':    '\u1f42',
-    'u)\\':    '\u1f52',
-    'w)\\':    '\u1f62',
-    '*)\\a':   '\u1f0a',
-    '*)\\e':   '\u1f1a',
-    '*)\\h':   '\u1f2a',
-    '*)\\i':   '\u1f3a',
-    '*)\\o':   '\u1f4a',
-    '*)\\u':   '\u1f5a',
-    '*)\\w':   '\u1f6a',
-
-    # Rough breathing and acute accent
-    'a(/':     '\u1f05',
-    'e(/':     '\u1f15',
-    'h(/':     '\u1f25',
-    'i(/':     '\u1f35',
-    'o(/':     '\u1f45',
-    'u(/':     '\u1f55',
-    'w(/':     '\u1f65',
-    '*(/a':    '\u1f0d',
-    '*(/e':    '\u1f1d',
-    '*(/h':    '\u1f2d',
-    '*(/i':    '\u1f3d',
-    '*(/o':    '\u1f4d',
-    '*(/u':    '\u1f5d',
-    '*(/w':    '\u1f6d',
-
-    # Rough breathing and grave accent
-    'a(\\':    '\u1f03',
-    'e(\\':    '\u1f13',
-    'h(\\':    '\u1f23',
-    'i(\\':    '\u1f33',
-    'o(\\':    '\u1f43',
-    'u(\\':    '\u1f53',
-    'w(\\':    '\u1f63',
-    '*(\\a':   '\u1f0b',
-    '*(\\e':   '\u1f1b',
-    '*(\\h':   '\u1f2b',
-    '*(\\i':   '\u1f3b',
-    '*(\\o':   '\u1f4b',
-    '*(\\u':   '\u1f5b',
-    '*(\\w':   '\u1f6b',
-
-    # Perispomeni
-    'a=':     '\u1fb6',
-    'h=':     '\u1fc6',
-    'i=':     '\u1fd6',
-    'u=':     '\u1fe6',
-    'w=':     '\u1ff6',
-
-    # Smooth breathing and perispomeni
-    'a)=':    '\u1f06',
-    'h)=':    '\u1f26',
-    'i)=':    '\u1f36',
-    'u)=':    '\u1f56',
-    'w)=':    '\u1f66',
-    '*)=a':   '\u1f0e',
-    '*)=h':   '\u1f2e',
-    '*)=i':   '\u1f3e',
-    '*)=w':   '\u1f6e',
-
-    # Rough breathing and perispomeni
-    'a(=':    '\u1f07',
-    'h(=':    '\u1f27',
-    'i(=':    '\u1f37',
-    'u(=':    '\u1f57',
-    'w(=':    '\u1f67',
-    '*(=a':   '\u1f0f',
-    '*(=h':   '\u1f2f',
-    '*(=i':   '\u1f3f',
-    '*(=u':   '\u1f5f',
-    '*(=w':   '\u1f6f',
-
-    # Perispomeni and ypogegrammeni
-    'a=|':    '\u1fb7',
-    'h=|':    '\u1fc7',
-    'w=|':    '\u1ff7',
-
-    # Ypogegrammeni
-    'a|':     '\u1fb3',
-    'h|':     '\u1fc3',
-    'w|':     '\u1ff3',
-    '*a|':    '\u1fbc',
-    '*h|':    '\u1fcc',
-    '*w|':    '\u1ffc',
-
-    # Acute accent and ypogegrammeni
-    'a/|':    '\u1fb4',
-    'h/|':    '\u1fc4',
-    'w/|':    '\u1ff4',
-
-    # Smooth breathing and ypogegrammeni
-    'a)|':    '\u1f80',
-    'h)|':    '\u1f90',
-    'w)|':    '\u1fa0',
-    '*)a|':   '\u1f88',
-    '*)h|':   '\u1f98',
-    '*)w|':   '\u1fa8',
-
-    # Rough breathing and ypogegrammeni
-    'a(|':    '\u1f81',
-    'h(|':    '\u1f91',
-    'w(|':    '\u1fa1',
-    '*(a|':   '\u1f89',
-    '*(h|':   '\u1f99',
-    '*(w|':   '\u1fa9',
-
-    # Smooth breathing, acute accent, and ypogegrammeni
-    'a)\|':   '\u1f82',
-    'h)\|':   '\u1f92',
-    'w)\|':   '\u1fa2',
-    '*)\\a|': '\u1f8a',
-    '*)\h|':  '\u1f9a',
-    '*)\w|':  '\u1faa',
-
-    # Rough breathing, grave accent, and ypogegrammeni
-    'a(\|':   '\u1f83',
-    'h)\|':   '\u1f93',
-    'w)\|':   '\u1fa3',
-    '*(\\a|': '\u1f8b',
-    '*)\h|':  '\u1f9b',
-    '*)\w|':  '\u1fab',
-
-    # Smooth breathing, accute accent, and ypogegrammeni
-    'a)/|':   '\u1f84',
-    'h)/|':   '\u1f94',
-    'w)/|':   '\u1fa4',
-    '*)/a|':  '\u1f8c',
-    '*)/h|':  '\u1f9c',
-    '*)/w|':  '\u1fac',
-
-    # Rough breating, acute accent, and ypogegrammeni
-    'a(/|':   '\u1f85',
-    'h(/|':   '\u1f95',
-    'w(/|':   '\u1fa5',
-    '*(/a|':  '\u1f8d',
-    '*(/h|':  '\u1f9d',
-    '*(/w|':  '\u1fad',
-
-    # Smooth breathing, ypogegrammeni, and perispomeni
-    'a)=|':   '\u1f86',
-    'h)=|':   '\u1f96',
-    'w)=|':   '\u1fa6',
-    '*)=a|':  '\u1f8e',
-    '*)=h|':  '\u1f9e',
-    '*)=w|':  '\u1fae',
-
-    # Rough breathing, ypogegrammeni, and perispomeni
-    'a(=|':   '\u1f87',
-    'h(=|':   '\u1f97',
-    'w(=|':   '\u1fa7',
-    '*(=a|':  '\u1f8f',
-    '*(=h|':  '\u1f9f',
-    '*(=w|':  '\u1faf',
-
-    # Diaeresis
-    'i+':     '\u03ca',
-    '*+i':    '\u03aa',
-    'i\\+':   '\u1fd2',
-    'i/+':    '\u1fd3',
-    'i+/':    '\u1fd3',
-    'i=+':    '\u1fd7',
-    'u+':     '\u03cb',
-    '*+u':    '\u03ab',
-    'u\\+':   '\u1fe2',
-    'u/+':    '\u1fe3',
-    'u=+':    '\u1fe7',
-
-    # Macron
-    'a&':     '\u1fb0',
-    'i&':     '\u1fd0',
-    'u&':     '\u1fe0',
-
-    # Breve
-    'a\'':    '\u1fb1',
-    'i\'':    '\u1fd1',
-    'u\'':    '\u1fe1',
-
-    # Basic punctuation
-    ':':      '\u00b7',
-    '\'':     '\u2019',
-    '-':      '\u2010',
-    '_':      '\u2014'
-}
+# Individual letter marks. Add s1, s2, and s3 separately from other one betacode
+# characters.
+_BETA_LETTERS = set('abgdezhqiklmncoprstufxyw')
+_BETA_LETTERS.add(('s1',))
+_BETA_LETTERS.add(('s2',))
+_BETA_LETTERS.add(('s3',))
+_BETA_LETTERS = frozenset(_BETA_LETTERS)
 
 
-def _create_unicode_map():
+def _create_unicode_map(combining):
     """
     Create the inverse map from unicode to betacode.
+
+    Args:
+    combining: Flag for how to treat combining codepoints. If set, then they are
+        converted into the appropriate betacode form. Otherwise, they are not
+        converted at all.
 
     Returns:
     The hash map to convert unicode characters to the beta code representation.
     """
     unicode_map = {}
 
-    for beta, uni in _BETACODE_MAP.items():
+    for beta, uni in _map.BETACODE_MAP.items():
         # Include decomposed equivalent where necessary.
         norm = unicodedata.normalize('NFD', uni)
         unicode_map[norm] = beta
         unicode_map[uni] = beta
 
     # Add the final sigmas.
-    final_sigma_norm = unicodedata.normalize('NFC', _FINAL_LC_SIGMA)
+    final_sigma_norm = unicodedata.normalize('NFD', _FINAL_LC_SIGMA)
     unicode_map[final_sigma_norm] = 's'
     unicode_map[_FINAL_LC_SIGMA] = 's'
 
+    if combining:
+        for beta, uni in _map.BETA_COMBINING_MAP.items():
+            unicode_map[uni] = beta
+
     return unicode_map
 
-_UNICODE_MAP = _create_unicode_map()
 
-def _create_conversion_trie():
+def _create_conversion_trie(case, combining, strict):
     """
     Create the trie for betacode conversion.
+
+    Args:
+    text: The beta code text to convert. All of this text must be betacode.
+    case: Case sensitivity, so that if set, only upper case betacode will be
+        converted.
+    combining: Flag to allow for combining characters in unicode output so that
+        any combination of diacritics and letters are allowed. Non-combining
+        forms will always be preferred.
+    strict: Flag to allow for flexible diacritic order on input.
 
     Returns:
     The trie for conversion.
     """
     t = pygtrie.CharTrie()
 
-    for beta, uni in _BETACODE_MAP.items():
-        # The order of accents is very strict and weak. Allow for many orders of
-        # accents between asterisk and letter or after letter. This does not
-        # introduce ambiguity since each betacode token only has one letter and
-        # either starts with a asterisk or a letter.
-        diacritics = beta[1:]
+    for beta, uni in _map.BETACODE_MAP.items():
+        if strict:
+            t[beta] = uni
+        else:
+            # The order of accents is very strict and weak. Allow for many orders of
+            # accents between asterisk and letter or after letter. This does not
+            # introduce ambiguity since each betacode token only has one letter and
+            # either starts with a asterisk or a letter.
+            diacritics = beta[1:]
 
-        perms = itertools.permutations(diacritics)
-        for perm in perms:
-            perm_str = beta[0] + ''.join(perm)
+            perms = itertools.permutations(diacritics)
+            for perm in perms:
+                perm_str = beta[0] + ''.join(perm)
 
-            t[perm_str.upper()] = uni
-            t[perm_str.lower()] = uni
+                t[perm_str.upper()] = uni
+
+                if not case:
+                    t[perm_str.lower()] = uni
+
+    if combining:
+        for beta, uni in _map.BETA_COMBINING_MAP.items():
+            t[beta] = uni
 
     return t
-
-_CONVERSION_TRIE = _create_conversion_trie()
 
 
 def _find_max_beta_token_len():
@@ -397,7 +106,7 @@ def _find_max_beta_token_len():
     longest single betacode token.
     """
     max_beta_len = -1
-    for beta, uni in _BETACODE_MAP.items():
+    for beta, uni in _map.BETACODE_MAP.items():
         if len(beta) > max_beta_len:
             max_beta_len = len(beta)
 
@@ -406,23 +115,40 @@ def _find_max_beta_token_len():
 _MAX_BETA_TOKEN_LEN = _find_max_beta_token_len()
 
 
-def beta_to_uni(text):
+_BETA_CONVERSION_TRIES = {}
+def beta_to_uni(text, case=False, combining=False, strict=False):
     """
     Converts the given text from betacode to unicode.
 
     Args:
     text: The beta code text to convert. All of this text must be betacode.
+    case: Case sensitivity, so that if set, only upper case betacode will be
+        converted.
+    combining: Flag to allow for combining characters in unicode output so that
+        any combination of diacritics and letters are allowed. Non-combining
+        forms will always be preferred.
+    strict: Flag to allow for flexible diacritic order on input.
 
     Returns:
     The converted text.
     """
-    t = _CONVERSION_TRIE
+    # Check if the requested configuration for conversion already has a trie
+    # stored otherwise convert it.
+    param_key = (case, combining, strict)
+    try:
+       t = _BETA_CONVERSION_TRIES[param_key]
+    except KeyError:
+        t = _create_conversion_trie(*param_key)
+        _BETA_CONVERSION_TRIES[param_key] = t
 
     transform = []
     idx = 0
-
     possible_word_boundary = False
+    in_middle_beta = False
+    current_middle_transform = ['']
+
     while idx < len(text):
+        # TODO: Check if this logic works with many combining characters.
         if possible_word_boundary and len(transform) > 1 and \
             transform[-2] == _MEDIAL_LC_SIGMA and not transform[-1].isalnum():
             transform[-2] = _FINAL_LC_SIGMA
@@ -430,15 +156,49 @@ def beta_to_uni(text):
         step = t.longest_prefix(text[idx:idx + _MAX_BETA_TOKEN_LEN])
 
         if step:
-            key, value = step
-            possible_word_boundary = text[idx] in _BETA_PUNCTUATION
+            if not in_middle_beta:
+                possible_word_boundary = text[idx] in _BETA_PUNCTUATION
+                key, value = step
 
-            transform.append(value)
-            idx += len(key)
+                # To work with combining characters we have to properly combine when
+                # there is a capital letter and the combining characters can be in
+                # front of the letter and have to be moved to behind. In this case,
+                # the prefix makes an actual match but the rest might not.
+                if combining and key[0] == '*':
+                    next_transform = ['']
+                    for letter in key[1:]:
+                        if letter in _BETA_LETTERS:
+                            next_transform[0] = t['*' + letter]
+                        else:
+                            next_transform.append(t[letter])
+                    transform.extend(new_transform)
+                else:
+                    transform.append(value)
+
+                idx += len(key)
+            else:
+                # If we have encoutered a combining betacode character with an
+                # unrecognized prefix, then keep track of its characters.
+                key, value = step
+                if key in _BETA_LETTERS:
+                    current_middle_transform[0] = t['*' + key]
+                    transform.extend(current_middle_transform)
+
+                    in_middle_beta = False
+                    current_middle_transform.clear()
+                else:
+                    current_middle_transform.append(value)
+
         else:
             possible_word_boundary = True
 
-            transform.append(text[idx])
+            # If the beginning of a combining capital betacode character is not
+            # recognized then keep track of it as we go.
+            if combining and text[idx] == '*':
+                in_middle_beta = True
+            else:
+                transform.append(text[idx])
+
             idx += 1
 
     # Check one last time in case there is some whitespace or punctuation at the
@@ -452,28 +212,36 @@ def beta_to_uni(text):
     converted = ''.join(transform)
     return converted
 
-def uni_to_beta(text):
+_UNI_CONVERSION_TRIES = {}
+def uni_to_beta(text, combining=True):
     """
     Convert unicode text to a betacode equivalent.
 
+    This method can handle tónos or oxeîa characters in the input.
+
     Args:
     text: The text to convert to betacode. This text does not have to all be
-          Greek polytonic text, and only Greek characters will be converted.
-          Note that in this case, you cannot convert to beta and then back to
-          unicode.
+        Greek polytonic text, and only Greek characters will be converted. Note
+        that in this case, you cannot convert to beta and then back to unicode.
+    combining: Flag that is set to encode combining codepoints into the betacode
+        equivalent.
 
     Returns:
     The betacode equivalent of the inputted text where applicable.
     """
+    param_key = (combining,)
+    try:
+        u = _UNI_CONVERSION_TRIES[param_key]
+    except KeyError:
+        u = _create_unicode_map(*param_key)
+        _UNI_CONVERSION_TRIES[param_key] = u
+
     transform = []
 
-    last_lookup_failed = False
     for ch in text:
         try:
-            last_lookup_failed = True
-            conv = _UNICODE_MAP[ch]
+            conv = u[ch]
         except KeyError:
-            last_lookup_failed = True
             conv = ch
 
         transform.append(conv)
